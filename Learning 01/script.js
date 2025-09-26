6e94da54b5506533f617a7dbf4e736cef6fb9d49
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-09-26 19:55:51 +0530

    taking lesson code not work in this

---
+++ Learning 01/script.js
@@ -0,0 +1,28 @@
+//  var let const
+var alpha;
+var a = 5;
+let beta;
+let a = 7;
+// const delta; xxxx it will give you error why ? declaration and intialization topic !
+const age = 19;
+// declaration and intialization topic 
+var d; //declaration
+var d = 5; // declare + intialize !
+// $notes: And same things happen in let keyword !
+
+//jo var a = 5 hai; 
+// 1}window mein add hota hai (future 1)
+// 2}function scooped hota hai (future 2)
+// 3}app firse declare kr skte ho same name se and error nahi ayega!
+
+// Reassignment, redeclartion
+
+var a = 12;
+a = 32;
+
+var a = 233; //aram se chalega |||redeclartion possible
+
+let gt = 12;
+b = 32;
+
+let gt = 433; // problem karega |||redeclartion not possible
