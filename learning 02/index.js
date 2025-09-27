e437dd648baff0f01aff5830a819807dfbfa93df
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-09-27 12:08:55 +0530

    day 2 of javscript

---
+++ learning 02/index.js
@@ -0,0 +1,18 @@
+// variable ko kaise bante hai 
+
+let name ="uzaif ansari"
+let age = 20;
+// console.log(name);
+// console.log(age);
+//above print in differnt line but below print in same line
+console.log(name,age); //it also can be done like this 
+age = 30; //yes it can be changed  
+console.log(age);
+
+const acc_no = 3429;
+
+console.log(acc_no);
+
+acc_no = 1234;
+console.log(acc_no);
+// so ut cannot be change after assigning which make it different from let
