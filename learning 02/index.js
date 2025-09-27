6d4c661d5cdded839c69687220c6cc330f9f630e
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-09-27 12:26:55 +0530

    learning var const and let

--- learning 02/index.js
+++ learning 02/index.js
@@ -1,18 +1,34 @@
 // variable ko kaise bante hai 
 
-let name ="uzaif ansari"
-let age = 20;
+// let name ="uzaif ansari"
+// let age = 20;
 // console.log(name);
 // console.log(age);
 //above print in differnt line but below print in same line
-console.log(name,age); //it also can be done like this 
-age = 30; //yes it can be changed  
-console.log(age);
+// console.log(name,age); //it also can be done like this 
+// age = 30; //yes it can be changed  
+// console.log(age);
 
-const acc_no = 3429;
+// let age = 30; 
+// >>> it give error same varible ko 2 times declare nhi karne dega that make differnt from var
 
-console.log(acc_no);
+// const acc_no = 3429;
 
-acc_no = 1234;
-console.log(acc_no);
+// console.log(acc_no);
+
+// acc_no = 1234;
+// console.log(acc_no);
 // so ut cannot be change after assigning which make it different from let
+// old tareeka
+// var _age = 40;
+// var _age = 50;
+// console.log(_age);
+// // yes dubara declare kiya possible hai sirf var me (jo ki old cheez thi)
+// if(true){
+//     var a = 35;
+//     // functional scope ko respect nhi karta tha
+// }
+// console.log(a);
+
+
+//this is all about let const and var!
