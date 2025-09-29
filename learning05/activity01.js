2bf16628d4aa211efc9103ec5bd7f1740b022b7c
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-09-29 23:51:57 +0530

    re-assignment and re-definiton

--- learning05/activity01.js
+++ learning05/activity01.js
@@ -1,6 +1,21 @@
-var age = 25;
+// var age = 25;   //cureently it is global
 
 
-if(true){
-    console.log(age);
-}
+// if(true){
+//     console.log(age);
+// }
+
+
+// function solve(){
+//     var age = 45;
+//     // console.log(age);  //1+ if make the comment of this and put in out side the block
+// }
+// // console.log(age); //1+ it will give erro
+// solve();
+
+
+var x = 5;
+var x = 78;
+//it will run = which bad move , and var is only that done that
+//re-assignment is possible, re definition is posssible
+
