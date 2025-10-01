227001c59561f48ce1189cc8665c271363a38e8e
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-10-01 10:13:31 +0530

    arithemetic operator - binary

---
+++ learning06/activity01.js
@@ -0,0 +1,14 @@
+//arithimetic operator , urinary and binary
+// +, -, *, /, %,  --,  ++,  **
+console.log("Hello JavaScript");
+
+let a = 32;
+let b = 4;
+// c = a + b;
+//all here are binary operator
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
