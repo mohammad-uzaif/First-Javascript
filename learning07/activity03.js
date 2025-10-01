cc6166af87a8ad16870fd0744a4e41871df0d586
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-10-01 11:34:21 +0530

    ternary operator

---
+++ learning07/activity03.js
@@ -0,0 +1,13 @@
+//strict Equality v/s loose Equality
+
+console.log('5' == 5);
+//output = true
+//here datatypes not matters
+
+// === LOOSE equality
+
+console.log('5' === 5);
+//output = false
+//here datatypes matters it should be same
+
+// === strict equality
---
+++ learning07/activity04.js
@@ -0,0 +1,6 @@
+//Ternary Operator
+
+let age = 13;
+let status1 = (age > 18) ? 'i can vote' : 'i can`t vote';
+
+console.log(status1);
