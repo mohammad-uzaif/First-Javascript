8954308e5bfe06c54db29b1856967445d1b7abbf
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-10-01 11:14:09 +0530

    comparison operator - consept

--- learning07/activity01.js
+++ learning07/activity01.js
@@ -4,3 +4,8 @@ let a = 5;
 a=a+10; //15
 a+=10;  //25
 console.log(a);
+
+
+let b = 44;
+b+=10;
+console.log(b);
---
+++ learning07/activity02.js
@@ -0,0 +1,14 @@
+// comparison operator
+//<,>,>=,<=,==,===,!==,!=
+
+// INTERVIEW QUESTION*********
+// strict Equality v/s loose Equality
+
+console.log(10>5);
+console.log(10<5);
+console.log(10>=5);
+console.log(10<=5);
+console.log(10==5);  //explain further
+console.log(10===5); //explain further
+console.log(10!==5);
+console.log(10!=5);
