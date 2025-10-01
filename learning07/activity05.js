5a3449bf2dc4926d693df8cafa15a0e1dbbc9484
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-10-01 17:32:46 +0530

    logical operator

---
+++ learning07/activity05.js
@@ -0,0 +1,15 @@
+// logical operators (comparison)
+// and &&, or ||, not !
+
+// #and && concept
+console.log(true && true);   //true
+// all condition should be correct then it give true
+console.log(true && false);   //false
+console.log(false && true);   //false
+
+// #or || concept
+console.log(true || false);   //true
+// one of the condition should correct then it give true
+console.log(false || true);   //true
+
+console.log(!false); //opposite
