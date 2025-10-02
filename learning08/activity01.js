3e4fb608170b25675ac820d8dad9bcf9471f8c35
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-10-02 15:38:02 +0530

    number & string conflict

---
+++ learning08/activity01.js
@@ -0,0 +1,12 @@
+//Number 
+
+let a = 5;
+let b = 34.7857;
+let c = 34.324325;
+
+console.log(a);
+console.log(b);
+console.log(c.toFixed(2)); //this make me define required decimal value 
+
+console.log(typeof(c));
+console.log(typeof(c.toFixed(2)));
