f98298ef3bde928d87c8a8fe4de7ab611a210645
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-10-04 12:40:41 +0530

    dates and ay in js

---
+++ learning12/at01.js
@@ -0,0 +1,11 @@
+const now = new Date();
+
+// console.log(now);
+
+console.log(now.toString());
+console.log(now.toLocaleString());
+console.log(now.getDay());
+console.log(now.getDate());
+console.log(now.getFullYear());
+console.log(now.getHours());
+console.log(now.getMonth());
