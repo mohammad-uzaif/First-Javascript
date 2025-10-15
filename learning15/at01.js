7cb983977776f7462822d1c0cef5d6fa8d5460b1
mohammad-uzaif <mohammaduzaif0111@gmail.com>
2025-10-16 02:33:43 +0530

    learning 15 at01

---
+++ learning15/at01.js
@@ -0,0 +1,13 @@
+// Number
+let a = 10;
+let b = 345.6871;
+console.log(b); //output 345.6871
+// we have method or function
+console.log(b.toFixed(2)); //output 345.68
+
+//now it is string or number
+console.log(typeof b);
+console.log(typeof b.toFixed(2));
+
+console.log(b.toPrecision(3));
+console.log(b.toString());
